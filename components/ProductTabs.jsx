3b5eb6a5b97763affe5762e65eb9cc0281352d0c
AllyintheCode <allyasadzade@gmail.com>
2026-04-21 12:46:59 +0400

    runtime bug ficed postcss.config.mjs

--- components/ProductTabs.jsx
+++ components/ProductTabs.jsx
@@ -1,28 +1,22 @@
 "use client";
-import React, { useEffect } from 'react'
-
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 export default function ProductTabs() {
-
   const [products, setProducts] = useState([]);
-  
-  
+
   useEffect(() => {
-  
-    axios.get("http://localhost/don-corleone-api/get_products.php")
+    axios
+      .get("http://localhost/don-corleone-api/get_products.php")
       .then((res) => {
-      
-        console.log("Gələn məlumat:", res.data); 
-        setProducts(res.data); 
+        console.log("Gələn məlumat:", res.data);
+        setProducts(res.data);
       })
       .catch((err) => {
         console.error("PHP linkində problem var:", err);
       });
   }, []);
 
-  
-  return (
-    
-    <div>ProductTabs</div>
-  )
+  return <div>ProductTabs</div>;
 }
